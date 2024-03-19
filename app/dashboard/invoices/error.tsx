5f2b2b150143dde36818ddf6fc9bf6b9f9776305
cjbellmyer <cjbellmyer5984@gmail.com>
2024-03-19 09:59:57 -0500

    added metadata

--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import { useEffect } from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Invoices",
+};
 
 export default function Error({
     error,
